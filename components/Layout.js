2726a02d95a17fe88bfe1e79ac17ad7a1bedf715
GuillermoTorrez <guillermo.torrez@gmail.com>
2021-03-17 20:49:08 -0400

    commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import Navbar from "../components/";
+import Navbar from "../components/Navbar";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import NProgress from "nprogress";
