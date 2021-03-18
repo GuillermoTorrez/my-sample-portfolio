89ea190b3a491f7b63b89b6514c9f8bcac0b3e88
GuillermoTorrez <guillermo.torrez@gmail.com>
2021-03-17 22:46:20 -0400

    commit

--- pages/post.js
+++ pages/post.js
@@ -1,19 +1,18 @@
 import Layout from "../components/Layout";
 import { useRouter } from "next/router";
-import { posts } from "../profile";
+import {posts} from "./profile";
 
 const Post = () => {
-
+  
+  console.log(posts)
   const router = useRouter();
 
   const currentPost = posts.filter(
     (e) => e.title === router.query.title
   )[0];
 
-  console.log(currentPost);
-
   return (
-    <Layout footer={false} title={currentPost.title}>
+    <Layout footer={false} title={currentPost.title}> {/* */}
     <div className="text-center">
         <img src={currentPost.imageURL} alt="" className="img-fluid" style={{width:'50%', height:'50%'}}/>
         <p>{currentPost.content}</p>
